0bba037ff17f90b4c743436639454c8920373b95
Sander Jansen <s.jansen@gmail.com>
2018-02-04 11:35:44 -0600

    cfox: remove all programs not needed for library only build

--- adie/help.h
+++
@@ -1,5 +0,0 @@
-/*********** Generated on 2020/08/24 21:44:21 by reswrap version 6.0.0 *********/
-
-/* Created by reswrap from file help.txt */
-extern const char help[];
-
--- chart/icons.h
+++
@@ -1,5 +0,0 @@
-/*********** Generated on 2020/08/24 21:43:59 by reswrap version 6.0.0 *********/
-
-/* Created by reswrap from file ../chart/marble.bmp */
-extern const unsigned char marble[];
-
